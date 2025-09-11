196f8266065622f2fa7d5a96e3dde268b2c50aa6
HR7-op <harsh.ray100@gmail.com>
2025-09-11 22:37:52 +0530

    modified: nav and sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,33 @@ import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
-  const { logout, authUser } = useAuthStore();
+	const { logout, authUser } = useAuthStore();
 
-  return (
-    <header
-      className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+	return (
+		<header
+			className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
     backdrop-blur-lg bg-base-100/80"
-    >
-      <div className="container mx-auto px-4 h-16">
-        <div className="flex items-center justify-evenly lg:justify-between h-full">
-          <div className="flex items-center gap-8">
-            <Link to="/conversations" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
-              <div className="size-8 rounded-lg bg-primary/10 flex items-center justify-center">
-                <img src="favicon.jpeg" alt="Advise AI Logo"className="w-5 h-5 text-primary" />
-              </div>
-              <h1 className="text-lg font-bold">Advise AI</h1>
-            </Link>
-          </div>
+		>
+			<div className="container mx-auto px-4 h-16">
+				<div className="flex items-center justify-evenly lg:justify-between h-full">
+					<div className="flex items-center gap-8">
+						<Link
+							to="/conversations"
+							className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+						>
+							<div className="size-8 rounded-lg bg-primary/10 flex items-center justify-center">
+								<img
+									src="favicon.jpeg"
+									alt="Advise AI Logo"
+									className="w-5 h-5 text-primary"
+								/>
+							</div>
+							<h1 className="text-lg font-bold">Advise AI</h1>
+						</Link>
+					</div>
 
-          <div className="flex items-center gap-2">
-            {/* <Link
+					<div className="flex items-center gap-2">
+						{/* <Link
               to={"/settings"}
               className={`
               btn btn-sm gap-2 transition-colors
@@ -33,23 +40,33 @@ const Navbar = () => {
               <span className="hidden sm:inline">Settings</span>
             </Link> */}
 
-            {authUser && (
-              <>
-                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
-                  <User className="size-5" />
-                  <span className="hidden sm:inline">Profile</span>
-                </Link>
+						{authUser && (
+							<>
+								<Link
+									to={"/profile"}
+									className={`btn btn-sm gap-2 hover:bg-green-800`}
+								>
+									<User className="size-5" />
+									<span className="hidden sm:inline">
+										Profile
+									</span>
+								</Link>
 
-                <button className="flex gap-2 items-center" onClick={logout}>
-                  <LogOut className="size-5" />
-                  <span className="hidden sm:inline">Logout</span>
-                </button>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+								<button
+									className="btn btn-sm flex gap-2 items-center hover:bg-green-800"
+									onClick={logout}
+								>
+									<LogOut className="size-5" />
+									<span className="hidden sm:inline">
+										Logout
+									</span>
+								</button>
+							</>
+						)}
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 };
 export default Navbar;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,87 +1,141 @@
 import { useEffect, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
-import { useAuthStore } from "../store/useAuthStore";
+// import { useAuthStore } from "../store/useAuthStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
+import { formatMessageTime } from "../lib/utils";
 
 const Sidebar = () => {
-  const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
+	const {
+		getInitialConversations,
+		criticalConversations,
+		conversations,
+		selectedConversation,
+		setSelectedConversation,
+		isConversationsLoading,
+	} = useChatStore();
 
-  const { onlineUsers } = useAuthStore();
-  const [showOnlineOnly, setShowOnlineOnly] = useState(false);
+	// const { onlineUsers } = useAuthStore();
+	const [showCriticalConversations, setShowCriticalConversations] =
+		useState(false);
 
-  useEffect(() => {
-    getUsers();
-  }, [getUsers]);
+	useEffect(() => {
+		getInitialConversations();
+	}, []);
 
-  const filteredUsers = showOnlineOnly
-    ? users.filter((user) => onlineUsers.includes(user.id))
-    : users;
+	const conversationsToShow = showCriticalConversations
+		? criticalConversations
+		: conversations;
 
-  if (isUsersLoading) return <SidebarSkeleton />;
+	if (isConversationsLoading) return <SidebarSkeleton />;
 
-  return (
-    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
-      <div className="border-b border-base-300 w-full p-5">
-        <div className="flex items-center gap-2">
-          <Users className="size-6" />
-          <span className="font-medium hidden lg:block">Contacts</span>
-        </div>
-        {/* TODO: Online filter toggle */}
-        <div className="mt-3 hidden lg:flex items-center gap-2">
-          <label className="cursor-pointer flex items-center gap-2">
-            <input
-              type="checkbox"
-              checked={showOnlineOnly}
-              onChange={(e) => setShowOnlineOnly(e.target.checked)}
-              className="checkbox checkbox-sm"
-            />
-            <span className="text-sm">Show online only</span>
-          </label>
-          <span className="text-xs text-zinc-500">({onlineUsers.length == 0 ? 0 : onlineUsers.length - 1} online)</span>
-        </div>
-      </div>
+	return (
+		<aside className="h-full w-20 lg:w-96 border-r border-base-300 flex flex-col transition-all duration-200">
+			<div className="border-b border-base-300 w-full p-5">
+				<div className="flex items-center gap-2">
+					<Users className="size-6" />
+					<span className="font-medium hidden lg:block">
+						Contacts
+					</span>
+				</div>
+				{/* TODO: Critical chats filter toggle */}
+				{/* <div className="mt-3 hidden lg:flex items-center gap-2">
+					<label className="cursor-pointer flex items-center gap-2">
+						<input
+							type="checkbox"
+							checked={showCriticalConversations}
+							onChange={(e) =>
+								setShowCriticalConversations(e.target.checked)
+							}
+							className="checkbox checkbox-sm"
+						/>
+						<span className="text-sm">
+							Show Critical Conversations
+						</span>
+					</label>
+					<span className="text-xs text-zinc-500">
+						({onlineUsers.length == 0 ? 0 : onlineUsers.length - 1}{" "}
+						online)
+					</span>
+				</div> */}
+			</div>
 
-      <div className="overflow-y-auto w-full py-3">
-        {filteredUsers.map((user) => (
-          <button
-            key={user.id}
-            onClick={() => setSelectedUser(user)}
-            className={`
+			<div className="overflow-y-auto w-full py-3">
+				{conversationsToShow?.map((conversation) => {
+					console.log(conversation);
+					return (
+						<button
+							key={conversation.id}
+							onClick={() =>
+								setSelectedConversation(conversation)
+							}
+							className={`
               w-full p-3 flex items-center gap-3
-              hover:bg-base-300 transition-colors
-              ${selectedUser?.id === user.id ? "bg-base-300 ring-1 ring-base-300" : ""}
+              hover:bg-green-800 transition-colors
+              ${
+					selectedConversation?.id === conversation.id
+						? "bg-stone-800 ring-1 ring-base-300"
+						: ""
+				}
             `}
-          >
-            <div className="relative mx-auto lg:mx-0">
-              <img
-                src={user.profilePic || "/avatar.png"}
-                alt={user.name}
-                className="size-12 object-cover rounded-full"
-              />
-              {onlineUsers.includes(user.id) && (
-                <span
-                  className="absolute bottom-0 right-0 size-3 bg-green-500 
+						>
+							<div className="relative mx-auto lg:mx-0">
+								<img
+									src={
+										conversation.profilePic || "/avatar.png"
+									}
+									alt={conversation.name}
+									className="size-12 object-cover rounded-full"
+								/>
+								{/* {onlineUsers.includes(conversation.id) && (
+								<span
+									className="absolute bottom-0 right-0 size-3 bg-green-500 
                   rounded-full ring-2 ring-zinc-900"
-                />
-              )}
-            </div>
+								/>
+							)} */}
+							</div>
 
-            {/* User info - only visible on larger screens */}
-            <div className="hidden lg:block text-left min-w-0">
-              <div className="font-medium truncate">{user.fullName}</div>
-              <div className="text-sm text-zinc-400">
-                {onlineUsers.includes(user.id) ? "Online" : "Offline"}
-              </div>
-            </div>
-          </button>
-        ))}
+							{/* User info - only visible on larger screens */}
+							<div className="hidden lg:block text-left min-w-0">
+								<div className="flex justify-between">
+									<span className="font-medium truncate w-1/2">{conversation.name}</span>
+									{conversation.last_message.message_text && (
+										<span className="text-xs text-zinc-500 ">
+											{formatMessageTime(
+												conversation.last_message
+													.provider_ts
+											)}
+										</span>
+									)}
+								</div>
 
-        {filteredUsers.length === 0 && (
-          <div className="text-center text-zinc-500 py-4">No online users</div>
-        )}
-      </div>
-    </aside>
-  );
+								<div>
+									{conversation.last_message.message_text && (
+										<p className="text-sm text-zinc-400 truncate w-64">
+											{
+												conversation.last_message
+													.message_text
+											}
+										</p>
+									)}
+								</div>
+								{/* <div className="text-sm text-zinc-400">
+								{onlineUsers.includes(user.id)
+									? "Online"
+									: "Offline"}
+							</div> */}
+							</div>
+						</button>
+					);
+				})}
+
+				{/* {criticalConversations.length === 0 && (
+					<div className="text-center text-zinc-500 py-4">
+						No critical conversations.
+					</div>
+				)} */}
+			</div>
+		</aside>
+	);
 };
 export default Sidebar;
