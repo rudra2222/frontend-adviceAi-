93a3d2868a169c3a0e95fdfb511a0c2037ec0737
HR7-op <harsh.ray100@gmail.com>
2025-09-28 00:18:35 +0530

    feat: added a "no messages yet" message on the chat container when there are no messages in a convo

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -54,6 +54,12 @@ const ChatContainer = () => {
 		<div className="flex-1 flex flex-col overflow-auto">
 			<ChatHeader />
 
+			{messages.length === 0 && (
+				<div className="flex-1 overflow-y-auto p-4 space-y-4 flex justify-center items-center text-stone-400">
+					No messages yet!
+				</div>
+			)}
+
 			{messages.length > 0 && (
 				<div className="flex-1 overflow-y-auto p-4 space-y-4">
 					{Object.entries(
