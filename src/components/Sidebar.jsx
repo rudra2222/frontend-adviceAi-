79793cd4b2297dccae7acf850db0a515d8bf1bdc
HR7-op <harsh.ray100@gmail.com>
2025-10-08 00:40:55 +0530

    chore: minor UI tweaks in Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -573,15 +573,15 @@ const Sidebar = () => {
 
     return (
         <aside className="h-full w-20 lg:w-96 border-r border-base-300 flex flex-col transition-all duration-200">
-            <div className="border-b border-base-300 w-full p-5">
+            {/* <div className="border-b border-base-300 w-full p-5">
                 <div className="flex items-center gap-2">
                     <Users className="size-6" />
                     <span className="font-medium hidden lg:block">
                         Contacts
                     </span>
                 </div>
-            </div>
-
+            </div> */}
+            
             {/* Search BOX */}
             <ChatSearchBox
                 value={searchQuery}
