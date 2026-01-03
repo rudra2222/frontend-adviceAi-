88f165526c479b41e8d1fbb26f528533daadfc77
rudra <rudravekariya020@gmail.com>
2026-01-03 13:55:01 +0530

    connection v2 to v1 in api

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
 	baseURL:
 		import.meta.env.VITE_MODE === "development"
-			? `${import.meta.env.VITE_LOCAL_URL}/api/v1`
-			: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
+			? `${import.meta.env.VITE_LOCAL_URL}/api/v2`
+			: `${import.meta.env.VITE_BACKEND_URL}/api/v2`,
 	withCredentials: true,
 });
